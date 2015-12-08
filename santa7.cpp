f351d0a27fdc0dd0f786e887771b7f8eac1d69b4
stevenStrij <ststrijakov@gmail.com>
2015-12-08 22:21:14 +1100

    Correct solution

--- santa7.cpp
+++ santa7.cpp
@@ -8,7 +8,6 @@
 #include <sstream>
 #include <stdio.h>
 
-
 static const unsigned int NOT = 0;
 static const unsigned int AND = 1;
 static const unsigned int OR = 2;
@@ -16,104 +15,80 @@ static const unsigned int RSHIFT = 3;
 static const unsigned int LSHIFT = 4;
 static const unsigned int LOAD = 5;
 
-typedef std::map<std::string, uint16_t> Memory;
+typedef std::map<std::string, std::string> Memory;
+typedef std::map<std::string, uint16_t> Result;
 
-bool is_number(const std::string& s)
-{
-    return !s.empty() && std::find_if(s.begin(), 
-            s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
-}
 
-void performOp(const unsigned int& op, Memory& registers,
-               const std::string& term1, const std::string& term2, const std::string& apply) {
+uint16_t eval(Memory& oper, Result& resMem,std::string curr) {
+    if (curr.find_first_not_of( "0123456789" ) == std::string::npos) {
+        return stoi(curr);
+    }
 
-    uint16_t num1, num2;
+    if (resMem.find(curr) != resMem.end()) {
+        return resMem[curr];
+    }
 
-    if (is_number(term1)) {
-        num1 = stoi(term1);
+    std::string toEval = oper[curr];
+
+    std::stringstream iss(toEval);
+    std::string ele;
+    std::vector<std::string> elements;
+
+    while (getline(iss, ele, ' ')) {
+        elements.push_back(ele);
+    }
+
+    if (elements[0] == "~") {
+        uint16_t res = eval(oper, resMem, elements[1]);
+        resMem[curr] = ~res;
+        
+        return ~res;
+    } else if (elements.size() == 1) {
+        return eval(oper, resMem, elements[0]);
     } else {
-        num1 = registers[term1];
-    }
+        uint16_t res1 = eval(oper, resMem, elements[0]);
+        uint16_t res2 = eval(oper, resMem, elements[2]);
 
-    if (is_number(term2)) {
-        num2 = stoi(term2);
-    } else {
-        num2 = registers[term2];
-    }
-
-    if (op == NOT) {
-        registers[apply] = ~num1;
-
-    } else if (op == AND) {
-        registers[apply] = num1 & num2;
-
-    } else if (op == OR) {
-        registers[apply] = num1 | num2;
-
-    } else if (op == LSHIFT) {
-        registers[apply] = num1 << num2;
-
-    } else if (op == RSHIFT) {
-        registers[apply] = num1 >> num2;
-
-    } else if (op == LOAD) {
-        registers[apply] = num1;
-    }
-}
-
-int main() {
-    std::smatch matches;
-    std::regex exp("([\\w ]+) -> (\\w+)");
-    std::string command;
-    std::smatch match;
-    Memory registers;
-
-    std::string applyTo;
-    std::string operation;
-    std::vector<std::string> tokens;
-
-    std::fstream file("in");
-
-    while (getline(file, command)) {
-        while (std::regex_search(command, match, exp)) {
-            operation = match[1];
-            applyTo = match[2];
-            tokens.clear();
-
-            std::istringstream iss(operation);
-            copy(std::istream_iterator<std::string>(iss),
-                    std::istream_iterator<std::string>(),
-                    std::back_inserter(tokens));
-
-            if (tokens[0] == "NOT") {
-                performOp(NOT, registers, tokens[1], tokens[1], applyTo);
-
-            } else if(tokens[1] == "OR") {
-                performOp(OR, registers, tokens[0], tokens[2], applyTo);
-
-            } else if (tokens[1] == "AND") {
-                performOp(AND, registers, tokens[0], tokens[2], applyTo);
-
-            } else if (tokens[1] == "LSHIFT") {
-                performOp(LSHIFT, registers, tokens[0], tokens[2], applyTo);
-
-            } else if (tokens[1] == "RSHIFT") {
-                performOp(RSHIFT, registers, tokens[0], tokens[2], applyTo);
-
-            } else {
-                performOp(LOAD, registers, tokens[0], tokens[0], applyTo);
-
-            }
-
-
-            command = match.suffix().str();
+        if (elements[1] == "|") {
+            resMem[curr] = res1 | res2;
+            return res1 | res2;
+        } else if (elements[1] == "&") {
+            resMem[curr] = res1 & res2;
+            return res1 & res2;
+        } else if (elements[1] == ">>") {
+            resMem[curr] = res1 >> res2;
+            return res1 >> res2;
+        } else if (elements[1] == "<<") {
+            resMem[curr] = res1 << res2;
+            return res1 << res2;
         }
     }
 
-    std::cout << "Registers contain: " << std::endl;
-    for (auto e = registers.begin(); e != registers.end(); ++e) {
-        std::cout << e->first << " -> " << e->second << std::endl;
+    return 0;
+}
+
+int main() {
+    std::ifstream file("in");
+    std::string line;
+    std::vector<std::string> tokens;
+
+    Memory memory;
+    Result res;
+
+    while (getline(file, line)) {
+        std::stringstream iss(line);
+        std::string token;
+        tokens.clear();
+
+        while (getline(iss, token, '=')) {
+            token.erase(token.begin(), std::find_if(token.begin(), token.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+            token.erase(std::find_if(token.rbegin(), token.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), token.end());
+
+            tokens.push_back(token);
+
+        }
+        memory[tokens[0]] = tokens[1];
     }
 
-    return 0;
+    std::cout << "Wire A has value " << eval(memory, res, "a");
 }
