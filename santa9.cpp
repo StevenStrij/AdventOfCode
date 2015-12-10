5091b687c2977d94c34df4d1118dd598ec74ae90
stevenStrij <ststrijakov@gmail.com>
2015-12-10 20:12:58 +1100

    Started 9

--- santa9.cpp
+++ santa9.cpp
@@ -6,22 +6,38 @@
 #include <iterator>
 #include <map>
 
-typedef std::pair<std::string, int> Edge;
+class Node {
+    std::string name;
+    std::map<std::string, int> edges;
+
+    Node() {}
+};
+
+class State {
+    std::string curr;
+    std::vector<std::string> seen;
+
+    int currCost;
+
+    State() { }
+};
 
 int main() {
     std::ifstream file("in");
     std::string line;
     std::vector<std::string> elements;
-    std::map<std::string, std::vector<std::pair<std::string, int> > > edges;
+    std::vector<Node> nodes;
 
     while (getline(file, line)) {
         std::istringstream iss(line);
+        elements.clear();
         copy(std::istream_iterator<std::string>(iss),
              std::istream_iterator<std::string>(),
              std::back_inserter(elements));
 
-        Edge e = std::make_pair(elements[2], stoi(elements[4]));
-        edges[elements[0]].push_back(e);
+        if (nodes.find(elements[0]) != nodes.end()) {
+
+        }
     }
 
     for (auto e = edges.begin(); e != edges.end(); ++e) {
