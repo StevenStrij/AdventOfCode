1514ac1d234d8da370e31eeaa5b54df5e1bdbff8
stevenStrij <ststrijakov@gmail.com>
2015-12-14 18:06:50 +1100

    Week 12 done

---
+++ C++/santa12.py
@@ -0,0 +1,36 @@
+import json
+
+data = ""
+
+fin = open("in", 'r')
+
+for line in fin:
+    data += line
+
+parsedJson = json.loads(data)
+
+
+def getNum(element):
+
+    sum = 0
+
+    if isinstance(element, dict):
+        for i in element:
+            if element[i] == "red":
+                return 0
+            else:
+                sum += getNum(element[i])
+
+    elif isinstance(element, list):
+        for i in element:
+            sum += getNum(i)
+
+    else:
+        if isinstance(element, int):
+            return (element)
+        else:
+            return 0
+
+    return sum
+
+print(getNum(parsedJson))
